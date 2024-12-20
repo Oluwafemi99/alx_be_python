67538a7629e4f666deb2a163c7ffe8720dadba44
oluwafemi <oluwafemidojumo@gmail.com>
2024-12-20 20:44:57 +0200

    daily remider

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,22 +1,22 @@
-Task = input("task description")
-Priority = input("enter task’s priority (high, medium, low)")
-Time_bound = input("is the task is time-bound? (yes or no)")
-match Priority:
+task = input("Enter your task")
+priority = input("priority (high/medium/low):")
+time_bound = input("is it time-bound? (yes or no)")
+match priority:
     case "high":
-        if Time_bound == "yes":
-            reminder = f"{Task} is a high priority task that requires immediate attention today!"
+        if time_bound == "yes":
+            reminder = f"{task} is a high priority task that requires immediate attention today!"
         else:
-            reminder = f"{Task} is a high priority task. make sure to complete it soon."
+            reminder = f"{task} is a high priority task. make sure to complete it soon."
     case "medium":
-        if Time_bound == "yes":
-            reminder = f"{Task} is a meduim priority task with a deadline. plan accodingly!"
+        if time_bound == "yes":
+            reminder = f"{task} is a meduim priority task with a deadline. plan accodingly!"
         else:
-            reminder = f"{Task} is a meduim priority task. Complete it when possible!"
+            reminder = f"{task} is a meduim priority task. Complete it when possible!"
     case "low":
-        if Time_bound == "yes":
-            reminder = f"{Task} is a low priority task with a deadline. consider scheduling time for it."
+        if time_bound == "yes":
+            reminder = f"{task} is a low priority task with a deadline. consider scheduling time for it."
         else:
-            reminder = f"{Task} is a low priority task. Consider completing it when you have free time"
+            reminder = f"{task} is a low priority task. Consider completing it when you have free time"
     case _:
         reminder = "invalid"      
 print (reminder)
