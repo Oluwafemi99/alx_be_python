8f447a4f16c2e35d3acb618d43798669b1be3bfc
oluwafemi <oluwafemidojumo@gmail.com>
2024-12-20 12:52:11 +0200

    weather

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,5 +1,4 @@
-current_weather = ("sunny", "rainy", "cold")
-weather = input("What's the weather like today? (sunny/rainy/cold):")
+weather = input("What's the weather like today? (sunny/rainy/cold):").lower()
 if weather == "sunny":
     print ("Wear a t-shirt and sunglasses.")
 elif weather == "rainy":
