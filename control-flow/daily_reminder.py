d1d59e4f7f1aafbc2ed64f89d2500024c78a2899
oluwafemi <oluwafemidojumo@gmail.com>
2024-12-20 21:14:35 +0200

    daily remider

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,19 +4,19 @@ time_bound = input("Is it time-bound? (yes/no):")
 match priority:
     case "high":
         if time_bound == "yes":
-            reminder = f"{task} is a high priority task that requires immediate attention today!"
+            reminder = f"Reminder: '{task}' is a high priority task that requires immediate attention today!"
         else:
-            reminder = f"{task} is a high priority task. make sure to complete it soon."
+            reminder = f"Reminder: '{task}' is a high priority task. make sure to complete it soon."
     case "medium":
         if time_bound == "yes":
-            reminder = f"{task} is a meduim priority task with a deadline. plan accodingly!"
+            reminder = f"Reminder: '{task}' is a meduim priority task with a deadline. plan accodingly!"
         else:
-            reminder = f"{task} is a meduim priority task. Complete it when possible!"
+            reminder = f"Reminder: '{task}' is a meduim priority task. Complete it when possible!"
     case "low":
         if time_bound == "yes":
-            reminder = f"{task} is a low priority task with a deadline. consider scheduling time for it."
+            reminder = f"Reminder: '{task}' is a low priority task with a deadline. consider scheduling time for it."
         else:
-            reminder = f"{task} is a low priority task. Consider completing it when you have free time"
+            reminder = f"Reminder: '{task}' is a low priority task. Consider completing it when you have free time"
     case _:
         reminder = "invalid"      
 print (reminder)
