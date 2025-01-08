68cd1d59390d53b882c7361c5d70886d1667d9c0
oluwafemi <oluwafemidojumo@gmail.com>
2025-01-09 01:16:08 +0200

    shopping list

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -4,21 +4,25 @@ shopping_list = []
 
 def add_item(item):
     shopping_list.append(item)
-    print(f"you have added {item}")
+    print(f"You have added {item}")
 
 
-def romove_item(item):
-    shopping_list.remove(item)
-    print(f"you have remove {item}")
+def remove_item(item):
+    if item in shopping_list:
+        shopping_list.remove(item)
+        print(f"You have removed {item}")
+    else:
+        print(f"{item} not found in the shopping list")
 
 
 def view_list():
+    print("Shopping List:")
     for item in shopping_list:
-        print(item)
+        print(f"- {item}")
 
 
 def display_menu():
-    print("Shopping List Manager")
+    print("\nShopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
@@ -32,12 +36,12 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            item = input("Enter the item to add:")
+            item = input("Enter the item to add: ")
             add_item(item)
         elif choice == '2':
             # Prompt for and remove an item
-            item = input("Enter the item to remove:")
-            romove_item(item)
+            item = input("Enter the item to remove: ")
+            remove_item(item)
         elif choice == '3':
             # Display the shopping list
             view_list()
