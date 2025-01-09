e356d685c3210432fe80cd6371a1c9179baec5ae
oluwafemi <oluwafemidojumo@gmail.com>
2025-01-09 03:12:45 +0200

    datetime

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timedelta
+
+
+def display_current_datetime():
+    current_date = datetime.now()
+    formatted_datetime = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"The current date and time is: {formatted_datetime}")
+
+
+display_current_datetime()
+
+
+def calculate_future_date():
+    days = int(input("Enter a number of days: "))
+    current_date = datetime.now()
+    future_date = current_date + timedelta(days=days)
+    formatted_future_date = future_date.strftime("%Y-%m-%d")
+    print(f"Future date: {formatted_future_date}")
+
+
+calculate_future_date()
