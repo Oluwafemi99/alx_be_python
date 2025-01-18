7c8763c096a59cd1063e27958b6cf1034ed216d7
oluwafemi <oluwafemidojumo@gmail.com>
2025-01-19 00:47:01 +0200

    test calculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,10 +7,16 @@ class testcalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_calculator(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
+
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 2), 3)
+
+    def test_multiply(self):
         self.assertEqual(self.calc.multiply(2, 3), 6)
+
+    def test_divide(self):
         self.assertEqual(self.calc.divide(4, 2), 2)
         self.assertEqual(self.calc.divide(4, 0), None)
 
