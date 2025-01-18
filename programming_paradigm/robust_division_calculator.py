da0acc3b4a1823030015af89fd01902ae7216fd5
oluwafemi <oluwafemidojumo@gmail.com>
2025-01-18 21:40:56 +0200

    robust calculator

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,5 +1,7 @@
 def safe_divide(numerator, denominator):
     try:
+        float(numerator)
+        float(denominator)
         result = numerator / denominator
     except ZeroDivisionError as e:
         print(e)
